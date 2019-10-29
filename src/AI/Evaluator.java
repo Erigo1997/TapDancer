9a29b995290f5912813cb56a5e846bc982743ea3
Erick <erick.lauridsen@gmail.com>
2019-10-29 14:53:08 +0100

    Git, specifying evaluation.

--- src/AI/Evaluator.java
+++ src/AI/Evaluator.java
@@ -1,10 +1,12 @@
 package AI;
 
+import Enumerators.COLOR;
 import Game.Board;
 
 public class Evaluator {
 
-    public int evaluateBoard(Board board) {
+    // Return evaluation. Add up value from own pieces, remove value for opponent pieces.
+    public int evaluateBoard(Board board, COLOR myColor) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 System.out.println(board.getPiece(j + 1, i + 1));
