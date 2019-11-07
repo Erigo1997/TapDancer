d8ba599a204f11f3e2abcf7e255a73216fad310b
Erigo1997 <32563911+Erigo1997@users.noreply.github.com>
2019-11-07 21:44:32 +0100

    Revert "Added evaluation for pawns."

--- src/AI/Evaluator.java
+++ src/AI/Evaluator.java
@@ -6,12 +6,10 @@ import Game.Board;
 public class Evaluator {
 
     // Return evaluation. Add up value from own pieces, remove value for opponent pieces.
-    public int evaluateBoard(Board board) {
+    public int evaluateBoard(Board board, COLOR myColor) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if(board.getPiece(j+1, i+1) == null)
-                    break;
-
+                System.out.println(board.getPiece(j + 1, i + 1));
             }
         }
         return 1;
